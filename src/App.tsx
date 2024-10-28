026902f6bfa7c39cbb2eff6a60cd1c708bc9d3a9
Julia Leal <julialeal.t@gmail.com>
2024-10-28 08:07:41 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from './components/ui/button'
+import './styles/globals.css'
+
+export function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Button>Enviar</Button>
+    </div>
+  )
+}
