d28d3705dfef558bbed5638db54d35f1e618a24a
Julia Leal <julialeal.t@gmail.com>
2024-10-31 20:54:59 -0300

    build: add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import './styles/globals.css'
 
-import { Button } from './components/ui/button'
+import { RouterProvider } from 'react-router-dom'
+
+import { router } from './routes'
 
 export function App() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <Button>Enviar</Button>
-    </div>
+    <RouterProvider router={router} />
   )
 }
