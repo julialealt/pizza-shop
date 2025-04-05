5d2a80ff76aa74ddec2ee6ca0731fa45e2b5691d
julialealt <julialeal.t@gmail.com>
2025-04-05 17:06:04 -0300

    feat: user auth

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,12 @@
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useMutation } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import * as zod from 'zod'
 
+import { signIn } from '@/api/sign-in'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
@@ -17,21 +19,25 @@ type SignInForm = zod.infer<typeof signInForm>
 
 export function SignIn() {
   const {
-    register, handleSubmit, formState: { isSubmitting },
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
   } = useForm<SignInForm>({
     resolver: zodResolver(signInForm),
   })
 
+  const { mutateAsync: authenticate } = useMutation({
+    mutationFn: signIn,
+  })
+
   async function handleSignIn(data: SignInForm) {
     try {
-      console.log(data)
-
-      await new Promise((resolve) => setTimeout(resolve, 2000))
+      await authenticate({ email: data.email })
 
       toast.success('Enviamos um link de autenticação para seu e-mail', {
         action: {
           label: 'Reenviar',
-          onClick: () => { handleSignIn(data) },
+          onClick: () => handleSignIn(data),
         },
       })
     } catch {
